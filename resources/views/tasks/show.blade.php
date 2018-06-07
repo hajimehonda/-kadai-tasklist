b00c61c6084b45d5ea036834d4e7c721d2f7e4f0
hajime honda <hajime.honda@rakuten.com>
2018-06-07 05:06:51 +0000

    CRUD messages

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <p>{{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
+
+@endsection
+
+
